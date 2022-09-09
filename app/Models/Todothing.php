f374f2ee88301c6905474bb1696e8ee91e6338ce
masaya <masaya.reon@gmail.com>
2022-09-09 14:59:54 +0900

    user固有の情報を表示させることが出来ました

--- app/Models/Todothing.php
+++ app/Models/Todothing.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Todothing extends Model
 {
@@ -12,12 +13,25 @@ class Todothing extends Model
     protected $fillable=[
         "deadline",
         "title",
-        "detail"
+        "detail",
+        "user_id"
     ];
+/*user_idを追加した。こうしないとcreateの時にuser_idの
+データを送信できない。 */
+
     public $timestamps = false;
 
     public static function getOrderBy()
   {
     return self::orderBy('deadline')->get();
   }
+/*順番を入れ替えるだけ */
+
+
+
+  public static function getOrderByAndId(){
+    $user_id=Auth::id();
+    return self::where("user_id",$user_id)->orderBy("deadline")->get();
+  }
+  /*user_idが一致するかつ順番を入れ替える */
 }
